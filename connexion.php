3fb94cccec47b4d3e0c81a842bfc5b638c2613e3
kamalsanens-sketch <kamalsanens@gmail.com>
2026-05-04 21:20:01 +0100

    Add files via upload

--- connexion.php
+++ connexion.php
@@ -1,11 +1,11 @@
 <?php
-$host = "0.0.0.0";
+$host = "localhost";
 $bdd  = "Vente";
 $user = "root";
-$pass = "root";
+$pass = "";
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$bdd", $user, $pass);
+    $pdo = new PDO("mysql:host=$host;dbname=$bdd;charset=utf8", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Connexion échouée : " . $e->getMessage());
